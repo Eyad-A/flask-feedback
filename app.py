07446c48c5e9d3348d8d5921a8e25ddb162833b1
EYAD ALSHAFI <EYAD.ALSHAFI@GMAIL.COM>
2021-03-18 16:19:52 -0500

    add heroku DB

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask_debugtoolbar import DebugToolbarExtension
 from models import connect_db, db, User, Feedback 
 from forms import RegisterForm, LoginForm, FeedbackForm, DeleteForm 
 from werkzeug.exceptions import Unauthorized
+import os
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///feedback"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = "abc123"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "postgres:///feedback")
 
 connect_db(app)
 db.create_all()
